5b1be033559cddb3eed399b8988861c02827ebd1
Shubham Singh <shubhamsingh7167424@gmail.com>
2026-01-02 14:54:15 +0530

    added ele3 in Product pom class

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepository/Products.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepository/Products.java
@@ -6,4 +6,7 @@ import org.openqa.selenium.support.FindBy;
 public class Products{
 	@FindBy (xpath = "//img[@alt='Create Product...']")
 	private WebElement createProductImageBtn;
+	
+	@FindBy (xpath = "//input[@value=' Search Now ']")
+	private WebElement ele3;
 }
