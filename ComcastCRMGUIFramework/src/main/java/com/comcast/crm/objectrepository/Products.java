a2feb4ea092e5127fe0a2e8f449e9ba9c54dca67
Shubham Singh <shubhamsingh7167424@gmail.com>
2026-01-02 14:50:21 +0530

    added ele2 in PRODUCTS pom class

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepository/Products.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepository/Products.java
@@ -6,4 +6,7 @@ import org.openqa.selenium.support.FindBy;
 public class Products{
 	@FindBy (xpath = "//img[@alt='Create Product...']")
 	private WebElement createProductImageBtn;
+	
+	@FindBy (name = "search_field")
+	private WebElement ele2;
 }
