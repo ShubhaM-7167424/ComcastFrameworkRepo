17fda907f7336724816fb71123afe9faf23fa961
Shubham Singh <shubhamsingh7167424@gmail.com>
2026-01-02 14:16:02 +0530

    Products, Added to POM class

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepository/Products.java
@@ -0,0 +1,9 @@
+package com.comcast.crm.objectrepository;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products{
+	@FindBy (xpath = "//img[@alt='Create Product...']")
+	private WebElement createProductImageBtn;
+}
